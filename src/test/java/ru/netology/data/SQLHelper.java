6849f12a7b4210b706b36dd97db7eb04a280dd18
Andrey Maximenko <andrey.v.maximenko@gmail.com>
2023-08-29 11:43:48 +0300

    change test, gradle-wrapper.properties, build.gradle

--- src/test/java/ru/netology/data/SQLHelper.java
+++ src/test/java/ru/netology/data/SQLHelper.java
@@ -21,7 +21,7 @@ public class SQLHelper {
 
     @SneakyThrows
     public static DataHelper.VerificationCode getVerificationCode() {
-        var codeSQL = "SELECT code FROM auth_codes ORDER by LIMIT 1";
+        var codeSQL = "SELECT code FROM auth_codes ORDER by created DESC LIMIT 1";
         var conn = getConn();
         var code = runner.query(conn, codeSQL, new ScalarHandler<String>());
         return new DataHelper.VerificationCode(code);
